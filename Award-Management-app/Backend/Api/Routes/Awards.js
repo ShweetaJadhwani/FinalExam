e6054778efea632ff19ecf9129b0d001c81b4f87
shweeta jadhwani <shweetajadhwani@gmail.com>
2025-01-03 15:45:12 +0500

    Added the endpoints

--- Award-Management-app/Backend/Api/Routes/Awards.js
+++ Award-Management-app/Backend/Api/Routes/Awards.js
@@ -8,12 +8,37 @@ router.get('/',(req, res, next) =>{
         message: 'Awards-Api'
     })
 })
+router.add('/',(req, res, next) =>{
+    res.status(200).json({
+        message: 'Awards Added'
+    })
+})
 
 router.post('/',(req, res, next) =>{
     res.status(201).json({
-        message: 'Hello  you there'
+        message: 'Awards posted'
     })
 })
+
+router.get('/',(req, res, next) =>{
+    res.status(200).json({
+        message: 'Retrieving the awards'
+    })
+})
+
+router.update('/',(req, res, next) =>{
+    res.status(201).json({
+        message: 'updates the existing awards'
+    })
+})
+router.delete('/',(req, res, next) =>{
+    res.status(201).json({
+        message: 'Delete an award from the database'
+    })
+})
+
+
+
 
 app.use((req, res, next)=>{
     const error = new error('not found');
